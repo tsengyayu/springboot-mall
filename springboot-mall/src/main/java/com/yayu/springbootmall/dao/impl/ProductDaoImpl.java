88c8c22eef60f131375112e5a88fe46587ae4e7b
tsengyayu <95689774+tsengyayu@users.noreply.github.com>
2024-06-30 00:43:44 +0800

    商品功能-實作查詢商品列表（分頁）

--- springboot-mall/src/main/java/com/yayu/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/yayu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -101,6 +101,8 @@ public class ProductDaoImpl implements ProductDao {
                 " created_date, last_modified_date " +
                 "FROM product WHERE 1=1";
         Map<String, Object> map = new HashMap<>();
+
+        //查詢條件
         if(productQueryParams.getCategory() != null){
             sql = sql + " AND category= :category";
             map.put("category",productQueryParams.getCategory().name());
@@ -109,7 +111,14 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search";
             map.put("search", "%"+productQueryParams.getSearch()+"%");
         }
+
+        //排序
         sql = sql + " ORDER BY "+productQueryParams.getOrderBy()+" "+productQueryParams.getSort();
+
+        //分頁
+        sql = sql +" LIMIT :limit OFFSET :offset";
+        map.put("limit",productQueryParams.getLimit());
+        map.put("offset",productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
