64b8deea24ac71fe86faba3cffe734e5cb218c6c
tsengyayu <95689774+tsengyayu@users.noreply.github.com>
2024-06-30 14:40:32 +0800

    帳號功能-檢查註冊的email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/yayu/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/yayu/springbootmall/service/impl/UserServiceImpl.java
@@ -4,18 +4,34 @@ import com.yayu.springbootmall.dao.UserDao;
 import com.yayu.springbootmall.dto.UserRegisterRequest;
 import com.yayu.springbootmall.model.User;
 import com.yayu.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
-        return userDao.createUser(userRegisterRequest);
+
+        //檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            logger.warn("該email {} 已被註冊",userRegisterRequest.getEmail());
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }else{
+            //創建帳號
+            return userDao.createUser(userRegisterRequest);
+        }
+
     }
 
     @Override
